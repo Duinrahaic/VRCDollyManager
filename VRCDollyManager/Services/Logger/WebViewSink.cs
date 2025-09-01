c21254b91748cd9e8f9044ad021ec8dfea8ba750
Duinrahaic <gman2306@gmail.com>
2025-09-01 08:58:17 -0500

    Added Logging for Webview Console

---
+++ VRCDollyManager/Services/Logger/WebViewSink.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Web.WebView2.Core;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace VRCDollyManager.Services.Logger;
+
+public class AppConsoleLogger(CoreWebView2 webView) : ILogEventSink
+{
+    public void Emit(LogEvent logEvent)
+    {
+
+        string level = logEvent.Level switch
+        {
+            LogEventLevel.Verbose => "debug",
+            LogEventLevel.Debug => "debug",
+            LogEventLevel.Information => "info",
+            LogEventLevel.Warning => "warn",
+            LogEventLevel.Error => "error",
+            LogEventLevel.Fatal => "error",
+            _ => "log"
+        };
+
+        string message = logEvent.RenderMessage();
+        if (logEvent.Exception != null)
+            message += $" | Exception: {logEvent.Exception}";
+
+        var escaped = message.Replace("\\", "\\\\").Replace("\"", "\\\"");
+
+        string script = $"console.{level}(\"[Serilog] {escaped}\");";
+
+        // fire-and-forget (don’t block logging pipeline)
+        _ = webView.ExecuteScriptAsync(script);
+    }
+}
